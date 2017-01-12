26652e923a299ed9befef608701d1bf0730eb083
fabriciojunior7 <fabriciovcjr@hotmail.com>
2017-01-12 14:35:52 -0200

    Add files via upload

--- python/funcoes.py
+++ python/funcoes.py
@@ -12,6 +12,14 @@ class Funcao(object):
         self.respostas = file.read().split("\n")
         file.close()
 
+        file = open("AI/funcoes/abrir/bloco_de_notas.txt", "r")
+        self.bloco_de_notas = file.read().split("\n")
+        file.close()
+
+        file = open("AI/funcoes/abrir/calculadora.txt", "r")
+        self.calculadora = file.read().split("\n")
+        file.close()
+
         file = open("AI/funcoes/abrir/navegador.txt", "r")
         self.navegador = file.read().split("\n")
         file.close()
@@ -37,6 +45,16 @@ class Funcao(object):
             if (e in texto):
                 print(self.respostas[random.randint(0, len(self.respostas)-1)])
 
+                for ee in self.bloco_de_notas:
+                    if(ee in texto):
+                        print("Abrindo o Bloco de Notas...")
+                        os.system("start notepad.exe")
+                        break
+                for ee in self.calculadora:
+                    if(ee in texto):
+                        print("Abrindo o Calculadora...")
+                        os.system("start calc.exe")
+                        break
                 for ee in self.navegador:
                     if (ee in texto):
                         print("Abrindo o Navegador...")
